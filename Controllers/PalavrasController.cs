761ed7ce690a60bf08c5a21ad75999b044613107
Bruno Sajermann <contato@sajermann.com>
2021-03-03 13:13:18 -0300

    Initial commit

---
+++ Controllers/PalavrasController.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MimicApi.Database;
+using MimicApi.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MimicApi.Controllers
+{
+    [Route("api/palavras")]
+    public class PalavrasController : ControllerBase
+    {
+        private readonly MimicContext _banco;
+        public PalavrasController(MimicContext banco)
+        {
+            _banco = banco;
+        }
+
+        //App -- /api/palavras
+        [Route("")]
+        [HttpGet]
+        public ActionResult ObterTodas()
+        {
+            return Ok(_banco.Palavras);
+        }
+
+        //Web -- /api/palavras/1
+        [Route("{id}")]
+        [HttpGet]
+        public ActionResult Obter(int id)
+        {
+            return Ok(_banco.Palavras.Find(id));
+        }
+
+        //-- /api/palavras(POST: id, nome, pontuacao)
+        [Route("")]
+        [HttpPost]
+        public ActionResult Cadastrar([FromBody] Palavra palavra)
+        {
+            _banco.Palavras.Add(palavra);
+            _banco.SaveChanges();
+            return Ok();
+        }
+
+        //-- /api/palavras/1 (PUT: id, nome, pontuacao, ativo, criacao)
+        [Route("{id}")]
+        [HttpPut]
+        public ActionResult Atualizar(int id, [FromBody] Palavra palavra)
+        {
+            palavra.Id = id;
+            _banco.Palavras.Update(palavra);
+            _banco.SaveChanges();
+            return Ok();
+        }
+
+        //-- /api/palavras/1
+        [Route("{id}")]
+        [HttpDelete]
+        public ActionResult Deletar(int id)
+        {
+            var palavra = _banco.Palavras.Find(id);
+            palavra.Ativo = false;
+            _banco.Palavras.Update(palavra);
+            _banco.SaveChanges();
+            return Ok();
+        }
+    }
+}
---
+++ Models/MimicContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MimicApi.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MimicApi.Database
+{
+    public class MimicContext : DbContext
+    {
+        public MimicContext(DbContextOptions<MimicContext>options) : base(options)
+        {
+
+        }
+
+        public DbSet<Palavra> Palavras { get; set; }
+    }
+}
